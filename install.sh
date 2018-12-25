629fc9f58b5cb3ae5793cb8228d541e202cb9f83
Zack Piper <zack@apertron.com>
2018-12-25 00:12:40 +0000

    ok
    Signed-off-by: Zack Piper <zack@apertron.com>

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# :(
+curl -s  https://raw.githubusercontent.com/Xorkle/it2i/master/install_utils.sh | bash
+curl -s  https://raw.githubusercontent.com/Xorkle/it2i/master/profile | tee -a /etc/profile
